c327b9691d403e2385ec79ecf7e4f529d95d68fa
Sara Pansarini <88805613+sarapansarini@users.noreply.github.com>
2021-09-02 22:49:56 -0300

    create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+/* abrir e fechar o menu*/
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+    element.addEventListener('click', function() {
+        nav.classList.toggle('show')
+    })
+}
+
+/*click em item do menu */ 
+
+const links = document.querySelectorAll ('nav ul li a')  
+
+for (const link of links) {
+    link.addEventListener('click', function() {
+        nav.classList.remove('show')
+})
+}
+
