827b85800450f6cd02876a8499ae63c26d5b45f6
Sara Pansarini <sara.pansarini@gmail.com>
2021-09-07 11:44:29 -0300

    wip aula 3

--- scripts.js
+++ scripts.js
@@ -17,3 +17,29 @@ for (const link of links) {
     nav.classList.remove("show");
   });
 }
+
+/* mudar o header da página quando der scroll */
+
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight; /*deslocamento*/
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= navHeight) {
+    //scroll é maior que a altura do header
+    header.classList.add("scroll");
+  } else {
+    // menos que a altura do header
+    header.classList.remove("scroll");
+  }
+});
+
+// testimonials slider
+
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
