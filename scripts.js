f4dab78aceff918fdda26f217dcaedd34711cd02
sarapansarini <sarapansarini@gmail.com>
2021-09-11 20:56:46 -0300

    Aula 05

--- scripts.js
+++ scripts.js
@@ -19,11 +19,10 @@ for (const link of links) {
 }
 
 /* mudar o header da página quando der scroll */
-
 const header = document.querySelector("#header");
 const navHeight = header.offsetHeight; /*deslocamento*/
 
-window.addEventListener("scroll", function () {
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     //scroll é maior que a altura do header
     header.classList.add("scroll");
@@ -31,7 +30,7 @@ window.addEventListener("scroll", function () {
     // menos que a altura do header
     header.classList.remove("scroll");
   }
-});
+}
 
 // testimonials slider
 
@@ -42,6 +41,12 @@ const swiper = new Swiper(".swiper-container", {
   },
   mousewheel: true,
   keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true,
+    },
+  },
 });
 
 // ScrollReveal: Mostrar elementos quando der scroll na página
@@ -60,7 +65,53 @@ scrollReveal.reveal(
 #services header, #services .card,
 #testimonials header, #testimonials .testimonials,
 #contact .text, #contact .links
+footer .brand, footer .social
 `,
 
   { interval: 100 }
 );
+
+// back to top button
+const backToTopButton = document.querySelector(".back-to-top");
+
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+// menu ativo conforme a seção visível na página
+const sections = document.querySelectorAll("main section[id]");
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4; // números encontrados por teste
+
+  for (const section of sections) {
+    const sectionTop = section.ofessetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  }
+}
+
+// when scroll
+
+window.addEventListener("scroll", function () {
+  changeHeaderWhenScroll();
+  backToTop();
+  activateMenuAtCurrentSection();
+});
